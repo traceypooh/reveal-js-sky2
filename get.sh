a872bf9eac66c86b2ea260d7d7f124f8ca33d26e
Tracey Jaquith <tracey_pooh@yahoo.com>
2017-07-01 20:09:57 -0700

    thinko fix

--- get.sh
+++ get.sh
@@ -8,18 +8,20 @@
 #    curl
 ##############################################################################
 
+DIR=$(echo $REPO |cut -f2 -d/);
+
 
 if [ ! -d reveal.js ]; then
     git clone https://github.com/hakimel/reveal.js;
 fi
 
-if [ ! -d reveal-js-sky2 ]; then
+if [ ! -d $DIR ]; then
     # try using SSH first;  else default back to HTTPS
     git clone git@github.com:$REPO.git  ||
     git clone https://github.com/$REPO;
 fi
 
-cd reveal-js-sky2;
+cd $DIR;
 
 (  sleep 3;  open http:localhost:8888  ) &
 ruby -run -e httpd . -p 8888;
